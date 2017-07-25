9777799e211fb996cbb5033852c13c64cef13544
Christopher Fujino <christopherfujino@gmail.com>
2017-07-25 09:51:36 -0700

    fleshed out documentation of API endpoints

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,6 +6,21 @@ const index = function (req, res) {
     endpoints: [
       {
         method: "GET", path: "/api", description: "Describes all available endpoints."
+      },
+      {
+        method: "GET", path: "/api/incidents", description: "Show all incidents in database."
+      },
+      {
+        method: "GET", path: "/api/incidents/:incident_id", description: "Show a particular incident."
+      },
+      {
+        method: "POST", path: "/api/incidents", description: "Create a new incident."
+      },
+      {
+        method: "PUT", path: "/api/incidents/:incident_id", description: "Update an incident."
+      },
+      {
+        method: "DELETE", path: "/api/incidents/:incident_id", description: "Destroy an incident."
       }
     ]
   });
