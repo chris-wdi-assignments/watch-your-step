7deaa345755fedf10e41c107026948a33f28ac54
Christopher Fujino <christopherfujino@gmail.com>
2017-07-25 09:54:33 -0700

    added: dummy controller methods

--- controllers/incidentsController.js
+++ controllers/incidentsController.js
@@ -1,5 +1,21 @@
 const index = function (req, res) {
-  res.json()
+  res.send("This will show all incidents.");
+}
+
+const show = function (req, res) {
+  res.send("this will show a single incident.");
+}
+
+const create = function (req, res) {
+  res.send("This will create a new incident");
+}
+
+const update = function (req, res) {
+  res.send("This will update a single incident.");
+}
+
+const destroy = function (req, res) {
+  res.send("This will destroy a single incident.");
 }
 
 module.exports = {
