1ab53b50ee201a03cba490a3536e05a5bb46b96f
Christopher Fujino <christopherfujino@gmail.com>
2017-07-25 13:43:25 -0700

    added: comments showing route

--- controllers/incidentsController.js
+++ controllers/incidentsController.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 
+// GET /api/incidents
 const index = function (req, res) {
   db.Incident.find({}, function(err, incidents){
     if (err){
@@ -9,18 +10,23 @@ const index = function (req, res) {
   });
 }
 
+// GET /api/incidents/:incident_id
 const show = function (req, res) {
+
   res.send("this will show a single incident.");
 }
 
+// POST /api/incidents
 const create = function (req, res) {
   res.send("This will create a new incident");
 }
 
+// PUT /api/incidents/:incident_id
 const update = function (req, res) {
   res.send("This will update a single incident.");
 }
 
+// DELETE /api/incidents/:incident_id
 const destroy = function (req, res) {
   res.send("This will destroy a single incident.");
 }
